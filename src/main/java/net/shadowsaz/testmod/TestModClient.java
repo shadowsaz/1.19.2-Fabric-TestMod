cc233e360457229d2a8ee3afd6ee36b0e96a70ba
shadowsaz <sazatoiaccess@gmail.com>
2022-11-03 23:18:40 +0100

    Add comment

--- src/main/java/net/shadowsaz/testmod/TestModClient.java
+++ src/main/java/net/shadowsaz/testmod/TestModClient.java
@@ -2,6 +2,7 @@ package net.shadowsaz.testmod;
 
 import net.fabricmc.api.ClientModInitializer;
 
+// Important comment
 public class TestModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
